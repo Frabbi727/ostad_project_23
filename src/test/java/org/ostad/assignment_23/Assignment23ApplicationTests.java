be021a649812202341bfc8d3c7100371c04f3bed
Rabbi <fazle.rabbi@bracits.com>
2026-01-28 21:37:56 +0600

    first commit

---
+++ src/test/java/org/ostad/assignment_23/Assignment23ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ostad.assignment_23;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assignment23ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
